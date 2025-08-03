d0c3db24000016021f1df9f08ae2a6a244bb0ff1
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2025-08-03 19:53:26 -0300

    2506. Count Pairs of Similar Strings - improve performance

--- Problems/Leet02506CountPairsOfSimilarStrings.cs
+++ Problems/Leet02506CountPairsOfSimilarStrings.cs
@@ -4,19 +4,19 @@ public class Leet02506CountPairsOfSimilarStrings
 {
     public int SimilarPairs(string[] words)
     {
+        var similarPairs = 0;
         var wordsBitValues = new Dictionary<int, int>();
-        for (int i = 0; i < words.Length; i++)
+        foreach (var word in words)
         {
             var currentWordBitValue = 0;
-            for (int j = 0; j < words[i].Length; j++)
+            foreach (char c in word)
             {
-                var letterBitValue = 1 << (words[i][j] - 'a');
+                var letterBitValue = 1 << (c - 'a');
                 currentWordBitValue |= letterBitValue;
             }
-            if (!wordsBitValues.TryAdd(currentWordBitValue, 1))
-                wordsBitValues[currentWordBitValue]++;
+            wordsBitValues.TryAdd(currentWordBitValue, 0);
+            similarPairs += wordsBitValues[currentWordBitValue]++;
         }
-        // binomial coefficient C(n = w.Value, k = 2)
-        return wordsBitValues.Sum(w => w.Value * (w.Value - 1) / 2);
+        return similarPairs;
     }
 }
