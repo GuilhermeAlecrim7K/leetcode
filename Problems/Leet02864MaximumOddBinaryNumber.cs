cf7e9564fc218dd85d6dacaf74c94dc26bb08721
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2025-08-11 23:23:03 -0300

    2864. Maximum Odd Binary Number - A more efficient solution

--- Problems/Leet02864MaximumOddBinaryNumber.cs
+++ Problems/Leet02864MaximumOddBinaryNumber.cs
@@ -2,7 +2,34 @@ namespace SharpLeetCode.Problems;
 
 public class Leet02864MaximumOddBinaryNumber
 {
-    public string MaximumOddBinaryNumber(string s)
+    public string MaximumOddBinaryNumberSecondAttempt(string s)
+    {
+        var n = s.Length;
+        var c = s.ToCharArray();
+        var lastOnBit = 0;
+        for (int i = 0, j = 1; j < n; j++)
+        {
+            if (c[i] == '1')
+            {
+                lastOnBit = i;
+                i++;
+                continue;
+            }
+            // c[i] == 0
+            if (c[j] == '1')
+            {
+                lastOnBit = i;
+                (c[i], c[j]) = (c[j], c[i]);
+                i++;
+            }
+        }
+        if (lastOnBit != n - 1)
+            (c[lastOnBit], c[n - 1]) = (c[n - 1], c[lastOnBit]);
+
+        return new string(c);
+    }
+
+    public string MaximumOddBinaryNumberVeryInneficient(string s)
     {
         var n = s.Length;
         var sb = new System.Text.StringBuilder(capacity: n);
