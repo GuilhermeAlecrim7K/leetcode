a91c5d6ee84b13249abd3ea936a21cf8f86f6c13
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2025-08-15 21:54:55 -0300

    1323. Maximum 69 Number

---
+++ Problems/Leet01323Maximum69Number.cs
@@ -0,0 +1,16 @@
+namespace SharpLeetCode.Problems;
+
+public class Leet01323Maximum69Number
+{
+    public int Maximum69Number(int num)
+    {
+        var digits = num.ToString();
+        var positionOfFirst6 = digits.IndexOf('6');
+        if (positionOfFirst6 < 0)
+            return num;
+
+        var newNumber = new Span<char>(digits.ToCharArray());
+        newNumber[positionOfFirst6] = '9';
+        return int.Parse(newNumber);
+    }
+}
