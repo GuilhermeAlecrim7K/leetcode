8e89fc066dbcf02e4684e602951bb1d0b0bac1b2
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2025-08-25 19:41:18 -0300

    1846. Maximum Element After Decreasing and Rearranging - A better approach

--- Problems/Leet01846MaximumElementAfterDecreasingAndRearranging.cs
+++ Problems/Leet01846MaximumElementAfterDecreasingAndRearranging.cs
@@ -5,12 +5,14 @@ public class Leet01846MaximumElementAfterDecreasingAndRearranging
     public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
     {
         int n = arr.Length;
-        Array.Sort(arr);
-        for (int i = 1; i < n; i++)
-        {
-            if (int.Abs(arr[i] - arr[i - 1]) > 1)
-                arr[i] = arr[i - 1] + 1;
-        }
-        return arr[n - 1];
+        var counts = new int[n + 1];
+        foreach (var num in arr)
+            counts[int.Min(num, n)]++;
+
+        var result = 1;
+        for (int i = 2; i < n + 1; i++)
+            result = int.Min(result + counts[i], i);
+
+        return result;
     }
 }
