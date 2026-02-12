be733061da630d88cb4a7eb91b3a1c7c4108c4ec
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2026-02-12 18:49:15 -0300

    2951. Find the Peaks

---
+++ Problems/Easy/Leet002951FindThePeaks.cs
@@ -0,0 +1,15 @@
+namespace SharpLeetCode.Problems.Easy;
+
+public class Leet002951FindThePeaks
+{
+    public IList<int> FindPeaks(int[] mountain)
+    {
+        var peaks = new List<int>();
+        for (int i = 1; i < mountain.Length -1; i++)
+        {
+            if (mountain[i] > mountain[i -1] && mountain[i] > mountain[i+1])
+                peaks.Add(i++);
+        }
+        return peaks;
+    }
+}
